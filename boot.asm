2eba1b1a7f912a732a98e85ed7dd7a0e89b9e714
Kshitij Patil <kshitijvpatil1703@gmail.com>
2025-03-31 13:52:29 +0530

    Work on protected mode and cleanup

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0 
-BITS 16 
+ORG 0x7c00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -8,55 +11,67 @@ _start:
 times 33 db 0 ; Visit: https://wiki.osdev.org/FAT : Offset 11 to 32 inclusive: size is 33
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
+    mov ss, ax
     mov sp, 0x7c00
-    sti
+    sti ;enable interrupts
 
-    mov ah, 2 ; Read sector command
-    mov al, 1 ; one sector to read
-    mov ch, 0 ; Cylinder low 8 bits
-    mov cl, 2 ; Read sector 2
-    mov dh, 0 ; head number 
-    mov bx, buffer ; ES:BX -> data buffer
-    int 0x13
-    jc error
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
-    
+; GDT
+
+gdt_start:
+get_null:
+    dd 0x0
+    dd 0x0
+
+; offset 8:
+gdt_code:      ; CS should point to this.
+    dw 0xffff  ; segment limit first 15 bits.
+    dw 0       ; base first 15 bits
+    db 0       ; base first 116-23 bits
+    db 0x9a    ; Access byte
+    db 11001111b ; High 4 bit flag and low 4 bit flag
+    db 0       ; base 24-31 bits
+
+; offset 10:
+gdt_data:      ; DS, SS, ES, FS, GS 
+    dw 0xffff  ; segment limit first 15 bits.
+    dw 0       ; base first 15 bits
+    db 0       ; base first 116-23 bits
+    db 0x92    ; Access byte
+    db 11001111b ; High 4 bit flag and low 4 bit flag
+    db 0       ; base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size of the descriptor
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-.loop:
-    mov ah, 0eh
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    int 0x10 
-    ret
-
-error_message: db 'Failed to load secotor' , 0
 
 times 510-($ - $$) db 0 
 dw 0xAA55
-
-buffer:
