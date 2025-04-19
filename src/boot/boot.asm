14fd9f80b274c1c3fb6c23ece7b9cdefac45f006
Kshitij Patil <kshitijvpatil1703@gmail.com>
2025-04-19 11:06:56 +0530

    Loading 32 bit kernel into memory and cleanup

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -8,19 +8,19 @@ _start:
     jmp short start
     nop
 
-times 33 db 0 ; Visit: https://wiki.osdev.org/FAT : Offset 11 to 32 inclusive: size is 33
+ times 33 db 0
 
 start:
     jmp 0:step2
 
 step2:
-    cli
+    cli ; Clear Interrupts
     mov ax, 0x00
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7c00
-    sti ;enable interrupts
+    sti ; Enables Interrupts
 
 .load_protected:
     cli
@@ -31,53 +31,101 @@ step2:
     jmp CODE_SEG:load32
 
 ; GDT
-
 gdt_start:
-get_null:
+gdt_null:
     dd 0x0
     dd 0x0
 
-; offset 8:
-gdt_code:      ; CS should point to this.
-    dw 0xffff  ; segment limit first 15 bits.
-    dw 0       ; base first 15 bits
-    db 0       ; base first 116-23 bits
-    db 0x9a    ; Access byte
-    db 11001111b ; High 4 bit flag and low 4 bit flag
-    db 0       ; base 24-31 bits
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
-; offset 10:
-gdt_data:      ; DS, SS, ES, FS, GS 
-    dw 0xffff  ; segment limit first 15 bits.
-    dw 0       ; base first 15 bits
-    db 0       ; base first 116-23 bits
-    db 0x92    ; Access byte
-    db 11001111b ; High 4 bit flag and low 4 bit flag
-    db 0       ; base 24-31 bits
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
 gdt_end:
 
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; size of the descriptor
+    dw gdt_end - gdt_start-1
     dd gdt_start
 
-[BITS 32]
-load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
+ [BITS 32]
+ load32:
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
 
-    ;Enable A20 line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
+ata_lba_read:
+    mov ebx, eax, ; Backup the LBA
+    ; Send the highest 8 bits of the lba to hard disk controller
+    shr eax, 24
+    or eax, 0xE0 ; Select the  master drive
+    mov dx, 0x1F6
+    out dx, al
+    ; Finished sending the highest 8 bits of the lba
 
-    jmp $
+    ; Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
+    ; Finished sending the total sectors to read
 
-times 510-($ - $$) db 0 
+    ; Send more bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
+    mov dx, 0x1F3
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send more bits of the LBA
+    mov dx, 0x1F4
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 8
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send upper 16 bits of the LBA
+    mov dx, 0x1F5
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 16
+    out dx, al
+    ; Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+; We need to read 256 words at a time
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw
+    pop ecx
+    loop .next_sector
+    ; End of reading sectors into memory
+    ret
+
+times 510-($ - $$) db 0
 dw 0xAA55
